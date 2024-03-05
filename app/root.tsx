d340c7f80f475a016ecc3189fca75fc6454e1f6b
Miguel Cardenas <miguelecsx@gmail.com>
2024-03-05 16:09:04 -0500

    feat: Improve the error page
    - Make use of the ErrorBoundary to handle bad requests

--- app/root.tsx
+++ app/root.tsx
@@ -8,8 +8,8 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
-
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -30,6 +30,33 @@ export default function App() {
         <Scripts />
         <LiveReload />
         <Analytics/>
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  console.error(error);
+  return (
+    <html>
+      <head>
+        <title>Oh no!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body className="bg-zinc-950 text-white font-sans">
+        <div className="max-w-6xl mx-auto pt-24 text-center">
+          <div className="text-4xl font-bold mb-4">Oh no! Something went wrong.</div>
+          <div className="text-base mb-8">
+            An unexpected error occurred. Please try again later or{" "}
+            <a href="/" className="text-blue-500 hover:underline font-bold">
+              return to the homepage
+            </a>
+            .
+          </div>
+        </div>
+        <Scripts />
       </body>
     </html>
   );
